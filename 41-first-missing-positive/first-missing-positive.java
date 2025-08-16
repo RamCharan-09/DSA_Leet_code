4b3b87908f02b2969637f64ea1a0506af86f5099
RamCharan-09 <ramcharanbarma99@gmail.com>
2025-08-16 16:22:27 +0530

    Time: 1 ms (100.00%) | Memory: 57.5 MB (41.89%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,13 +1,29 @@
 class Solution {
+    static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
     public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        // List<Integer> res = new ArrayList<>();
         int n = nums.length;
-        Arrays.sort(nums);
-        int j = 1;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == j){
-                j++;
+        while(i < n){
+            int cor = nums[i] - 1;
+
+            if(nums[i] > 0 && nums[i] <= n && nums[i] != nums[cor]){
+                swap(nums, i, cor);
+            }else{
+                i++;
             }
         }
-        return j;
+
+        
+        for(int j = 0; j < n; j++){
+            if(nums[j] != j+1){
+                return j+1;
+            }
+        }
+        return n+1;
     }
 }
