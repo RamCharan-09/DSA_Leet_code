01eb0c4f42d225dfce1be6fc5b800211272abf84
Barma Ram Charan <ramcharanbarma99@gmail.com>
2025-09-27 10:37:14 +0530

    Time: 7 ms (68.15%) | Memory: 41.9 MB (11.77%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.java
+++ 830-largest-triangle-area/largest-triangle-area.java
@@ -1,39 +1,18 @@
 class Solution {
-    public double largestTriangleArea(int[][] points) {
-        int n = points.length;
-        double max = 0;
-        
-        for (int i = 0; i < n; ++i) 
-            for (int j = i + 1; j < n; ++j)
-                for (int k = j + 1; k < n; ++k) {
-                    double area = area(points, i, j, k);
-                    if (area > max) {
-                        max = area;
-                    }
+    public double largestTriangleArea(int[][] p) {
+        int n = p.length;
+        double ans = 0.0;
+        for(int i=0;i<n-2;i++){
+            double x1=p[i][0], y1=p[i][1];
+            for(int j=i+1;j<n-1;j++){
+                double x2=p[j][0], y2=p[j][1];
+                for(int k=j+1;k<n;k++){
+                    double x3=p[k][0], y3=p[k][1];
+                    double area=0.5*Math.abs(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
+                    ans=Math.max(ans,area);
                 }
-        
-        
-        return max;
-    }
-    
-    // triangle
-    double area(int[][] points, int i, int j, int k) {
-        int[] p1 = points[i];
-        int[] p2 = points[j];
-        int[] p3 = points[k];
-        
-        double area = 0;
-        area += area(p1, p2);
-        area += area(p2, p3);
-        area += area(p3, p1);
-        
-        return Math.abs(area);
-    }
-    
-    // right trapezoid
-    double area(int[] p1, int[] p2) {
-        int w = p2[0] - p1[0];
-        double h = (p1[1] + p2[1] + 200) / 2.0;
-        return w * h;
+            }
+        }
+        return ans;
     }
 }
