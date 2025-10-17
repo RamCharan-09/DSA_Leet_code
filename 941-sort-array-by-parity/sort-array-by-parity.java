00e1a4bcc8deb24cae3bd67f59976b4258d5aee8
Barma Ram Charan <ramcharanbarma99@gmail.com>
2025-10-17 11:15:30 +0530

    Time: 1 ms (50.41%) | Memory: 45.8 MB (5.28%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        ArrayList<Integer> odd = new ArrayList<>();
+        ArrayList<Integer> even = new ArrayList<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] % 2 == 0) even.add(nums[i]);
+            else odd.add(nums[i]);
+        }
+        even.addAll(odd);
+
+        for(int i = 0; i < even.size(); i++){
+            nums[i] = even.get(i);
+        }
+        return nums;
+    }
+}
